2b6c2a6f677aff53fa46fa1ee14019dedd217a72
ihebKhodja <iheb.khodja@gmail.com>
2022-08-30 17:02:30 +0100

    new commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn todlistapp.wsgi --log-file -
--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8263m@-t*=*^447xz)ir!m^2l!oj9cwq^t=l-4i)my-b9^&-))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todlistapp.herokuapp.com',
+                 '127.0.0.1:8000']
 
 
 # Application definition
