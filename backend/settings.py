c783724747cf71c0bacc1502f426da5fc6a3a906
ihebKhodja <iheb.khodja@gmail.com>
2022-09-04 12:10:44 +0100

    change files

--- backend/settings.py
+++ backend/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8263m@-t*=*^447xz)ir!m^2l!oj9cwq^t=l-4i)my-b9^&-))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['todlistapp.herokuapp.com',
-                 '127.0.0.1:8000']
+ALLOWED_HOSTS = ['todlistapp.herokuapp.com','127.0.0.1:8000']
 
 
 # Application definition
